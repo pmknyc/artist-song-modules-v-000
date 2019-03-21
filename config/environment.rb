89b83893fc1360215a2a2096ae4ff9de3b4c4393
Paula McKinley <pmknyc@optimum.net>
2019-03-21 23:23:48 +0000

    Automatically backed up by Learn

--- config/environment.rb
+++ config/environment.rb
@@ -8,9 +8,15 @@ require 'pry'
 # As we build larger, more complex programs, it begins to make sense to handle
 #   all requirements in one place.
 
+<<<<<<< HEAD
 require_relative '../lib/concerns/findable'
 require_relative '../lib/concerns/memorable'
 require_relative '../lib/concerns/paramable'
+=======
+# require_relative '../lib/concerns/findable'
+# require_relative '../lib/concerns/memorable'
+# require_relative '../lib/concerns/paramable'
+>>>>>>> 67da0e0b7b3d97e90dcf0436b6f004a3c4dd9ce6
 
 require_relative '../lib/artist.rb'
 require_relative '../lib/song.rb'
